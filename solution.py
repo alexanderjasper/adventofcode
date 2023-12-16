1a5796143b7896590fe9e89b03a5808f29cbe45e
Alexander Skjold Jasper <askjoldjasper@gmail.com>
2023-12-16 07:42:11 +0100

    Add more structure to template

--- solution.py
+++ solution.py
@@ -1,5 +1,22 @@
 import os
 import sys
 
-with open(os.path.join(sys.path[0], "input.txt"), "r") as f:
-    lines = f.read().splitlines()
+
+def part1(puzzle_input):
+    return f'Part 1: '
+
+
+def part2(puzzle_input):
+    return f'Part 2: '
+
+
+def solve(input_file_path):
+    with open(os.path.join(sys.path[0], input_file_path), "r") as f:
+        lines = f.read().splitlines()
+        print(input_file_path)
+        print(part1(lines))
+        print(part2(lines))
+
+
+solve('test_input.txt')
+solve('input.txt')
